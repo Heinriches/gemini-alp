ad393a180978b50ebd230b0741245c6f170d3409
Heinriches <heydrich.institut@gmail.com>
2026-03-13 18:03:19 -0300

    javadoc

---
+++ Ex01/doc/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"<Unnamed>","c":"Ex01","l":"Ex01()","u":"%3Cinit%3E()"},{"p":"<Unnamed>","c":"Ex01","l":"main(String[])","u":"main(java.lang.String[])"}];updateSearchResults();
