0b97dbaccaa6438ecd28d7f88cd2e43eb276d0dc
Heinriches <heydrich.institut@gmail.com>
2026-03-13 17:04:23 -0300

    Create Ex01.java

---
+++ Ex01.java
@@ -0,0 +1,12 @@
+/*
+ * Exercício Gemini-01
+ * 
+ * Escreva um programa que:
+ * 	(1) Peça ao usuário um número-inteiro;
+ * 	(2) Utilize um método chamado 'isPar' para verificar se o número é par ou ímpar;
+ * 	(3) Utilize um método chamado 'gerarMensagem', que recebe o número e o resultado da
+ * 	verificação, retornando uma frase formatada;
+ * 	(4) Imprima o resultado na tela.
+ * 
+ * 	DICA TÉCNICA: o método 'isPar' deve retornar um tipo 'boolean' (verdadeiro ou falso).
+ */
